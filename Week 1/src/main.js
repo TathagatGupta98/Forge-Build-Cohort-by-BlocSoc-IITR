9cbe5a4a77358b879e9681619ecef33485156825
Tathagat <janaktathagat@gmail.com>
2026-01-03 14:41:06 +0000

    feat: Added a theme toggle button with simple jss and a sun/moon icon that switch via js (Closes #2, Closes #3)

--- Week 1/src/main.js
+++ Week 1/src/main.js
@@ -1,3 +1,21 @@
 import './style.css';
 
-console.log("TypeScript is connected and running!");
+const htmlTag = document.documentElement;
+const themeSwitchButton = document.getElementById('theme-switch');
+const moonIcon = document.getElementById('moon');
+const sunIcon = document.getElementById('sun');
+
+sunIcon.style.display = 'none';
+
+themeSwitchButton.addEventListener('click', () => {
+    htmlTag.classList.toggle('dark');
+    
+    if (sunIcon.style.display === 'none') {
+        sunIcon.style.display = '';
+        moonIcon.style.display = 'none';
+    }
+    else {
+        sunIcon.style.display = 'none';
+        moonIcon.style.display = '';
+    }
+});
