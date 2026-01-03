e2c1dbbf3f43f0ec54d82729f86ffd5121d81ae7
Tathagat <janaktathagat@gmail.com>
2026-01-03 19:16:06 +0000

    feat: Using simple Ethers.js added a button to connect to the wallet provider (Closes #4)

--- Week 1/src/main.js
+++ Week 1/src/main.js
@@ -1,9 +1,11 @@
 import './style.css';
+import { ethers } from "ethers";
 
 const htmlTag = document.documentElement;
 const themeSwitchButton = document.getElementById('theme-switch');
 const moonIcon = document.getElementById('moon');
 const sunIcon = document.getElementById('sun');
+const connectWalletButton = document.getElementById('connect-wallet');
 
 sunIcon.style.display = 'none';
 
@@ -19,3 +21,20 @@ themeSwitchButton.addEventListener('click', () => {
         moonIcon.style.display = '';
     }
 });
+
+let signer = null;
+let provider;
+
+connectWalletButton.addEventListener('click', async () => {
+
+    if (window.ethereum == null) {
+     console.log("MetaMask not installed; using read-only defaults")
+    provider = ethers.getDefaultProvider()
+
+    } else {    
+        provider = new ethers.BrowserProvider(window.ethereum);
+        signer = await provider.getSigner();
+    }
+    connectWalletButton.textContent = "Connected!";
+});
+
