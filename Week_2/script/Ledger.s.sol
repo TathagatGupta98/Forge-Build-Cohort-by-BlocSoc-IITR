f19576f607a2bdd24f77bbabaab98ce25da5a25c
Tathagat <janaktathagat@gmail.com>
2026-01-08 21:26:48 +0000

    Used script(forge script) to deploy the contract on sepolia network.

--- Week_2/script/Ledger.s.sol
+++ Week_2/script/Ledger.s.sol
@@ -1,6 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
-contract Ledger {
-    
-}
+import {Script} from "forge-std/Script.sol";
+import {Ledger} from "../src/Ledger.sol";
+
+contract LedgerDeploy is Script{
+    function run() external {
+        vm.startBroadcast();
+        new Ledger();
+        vm.stopBroadcast();
+    }
+} 
