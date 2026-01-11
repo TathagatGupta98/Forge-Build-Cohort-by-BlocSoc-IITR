3d632b713ee4f73d9ad84b1a3b1bf2b0e46a1686
Tathagat <janaktathagat@gmail.com>
2026-01-11 15:25:31 +0000

    feat: Added the function to withdraw funds that were deposited earlier

--- Week_2/src/Ledger.sol
+++ Week_2/src/Ledger.sol
@@ -7,10 +7,25 @@ contract Ledger {
 
     event NewDeposit(address user, uint256 amount);
 
-    function deposit(uint256 depositAmount) external  {
+    function deposit(uint256 depositAmount) public payable{
+
         address userAddress = msg.sender;
+
+        (bool success, ) = userAddress.call{value: depositAmount}("");
+        require(success, "Transfer failed.");
+
         balances[userAddress] += depositAmount;
+
         emit NewDeposit(userAddress, depositAmount);
+    }
+
+    function withdraw() public {
+        
+        address userAddress = msg.sender;
+        require(balances[userAddress] > 0, "Insufficient balance to withdraw.");
+
+        (bool success, ) = userAddress.call{value: balances[userAddress]}("");
+        require(success, "Transfer failed.");
     }
 
     function getBalance() external view returns (uint256) {
