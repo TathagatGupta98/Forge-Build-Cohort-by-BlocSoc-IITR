95b2af25bf9f654a7f106b10b144c004e39594c6
Tathagat <janaktathagat@gmail.com>
2026-01-17 14:33:18 +0000

    Wrote a simple ERC20 token logic to mint Fiat collateralized Stable Coins(1:1 Collateral). Connected the frontend using Wagmi Hooks. UseAccount and useConnect for connecting the account, useWriteContract for state change trasnsactions, useWaitForTrasactionReceipt for showing the spinner and diabling the button when the transactions is confirmed. User can straight up give collateral as EthSepolia to mint TSC and burn TSC for EthSepolia

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { type ReactNode } from "react";
+import { Providers } from "./providers";
+
+export const metadata: Metadata = {
+  title: "Stable Coin Exchange DApp",
+  description: "A decentralized application for exchanging stable coins built on Ethereum.",
+};
+
+export default function RootLayout(props: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <Providers>
+          {props.children}
+        </Providers>
+      </body>
+    </html>
+  );
+}
